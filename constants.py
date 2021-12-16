0d0814921bfd7d0edca221c9b9aa45ba799ad620
ilanlansh <ilan.shrir@gmail.com>
2021-12-16 09:43:30 +0200

    move Point and size constants from main to constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,7 @@
 from enum import Enum;
+from point import Point;
+
+import scipy.constants as spc;
 
 class Colors(Enum):
     '''
@@ -13,3 +16,16 @@ class Colors(Enum):
     DARK_GRAY: str = '#212121';
     DARK_RED: str = '#3D0103';
     ORANGE: str = '#FF8000';
+
+HEIGHT: int = 600;
+WIDTH:  int = 600;
+
+TOPMOST:    Point = Point(WIDTH * 0.5, 0);
+BOTTOMMOST: Point = Point(WIDTH * 0.5, HEIGHT);
+LEFTMOST:   Point = Point(0, HEIGHT * 0.5);
+RIGHTMOST:  Point = Point(WIDTH, HEIGHT * 0.5);
+
+CENTER: Point = Point(WIDTH * 0.5, HEIGHT * 0.5);
+RAYLENGTH: int = 250;
+
+SPEED_OF_LIGHT = spc.speed_of_light / 1000.0;
